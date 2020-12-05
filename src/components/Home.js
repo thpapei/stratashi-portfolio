cb61ed3297165a34575f0a0fe4a2fcbaa7db878d
Thomas Pappas <thpapei1@gmail.com>
2020-12-05 16:54:51 +0200

    Added content and styling to the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
 const Home = () => {
   return (
     <div className='home'>
-      <h1>Stratashi</h1>
+      <div className='content-container'>
+        <div className='content'>
+          <div className='content__item'>
+            <h1>I am Stratashi</h1>
+          </div>
+          <div className='content__item'>
+            <p>
+              I am a 27 year old visual artist based in
+              Greece. I started editing animation in music 13 years ago and
+              experimenting with VFX, animation and directing soon after I
+              attended Ι.Ε.Κ. ΑΚΜΗ and took certificate 8 years ago. Since
+              then  I have been working as an acrobat in a theater on
+              the side while doing music video and editing jobs as my
+              main job. My passion still goes strong while I am exploring
+              art every day through many roads that make my grateful for
+              the ride that we call life.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
