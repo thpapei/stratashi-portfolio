807c7bac06d2c82228bdd0d58a2628cc85dc952d
Thomas Pappas <thpapei1@gmail.com>
2020-12-22 15:26:34 +0200

    Added all images to the gallery

--- src/images/projects/images.js
+++ src/images/projects/images.js
@@ -1,36 +1,36 @@
 // images
 import aa from './project-images/aa.png';
-import secondtshirt from './project-images/secondtshirt0053.png';
-// import einsteinskeleton from './project-images/einstein-skeleton.png';
-// import finalmaskisabigbusiness from './project-images/finalMaskisabigbusiness.png';
-// import ghostinthesnow from './project-images/ghost_in_the_snow.jpeg';
-// import landtostandtoheart from './project-images/landtostandtoheart0432.jpg';
-// import movingonlookingaheadparachute from './project-images/movingonlookingaheadparachute.png';
-// import nhandsalot from './project-images/nhandsalot.png';
-// import nkjbk from './project-images/nkjbk.png';
-// import reddoor from './project-images/reddoor.jpg';
-// import redrunnerrunning from './project-images/redRunnerRunning.png';
-// import runthrough from './project-images/runthrough.png';
-// import sketchash from './project-images/sketchashCrazinessinsanity.png';
-// import tworl from './project-images/tworl.png';
-// import zebra from './project-images/zebratest4mask.png';
+import secondtshirt from './project-images/secondtshirt.png';
+import einsteinskeleton from './project-images/einstein-skeleton.png';
+import finalmaskisabigbusiness from './project-images/finalMaskisabigbusiness.png';
+import ghostinthesnow from './project-images/ghost_in_the_snow.jpeg';
+import landtostandtoheart from './project-images/landtostandtoheart0432.jpg';
+import movingonlookingaheadparachute from './project-images/movingonlookingaheadparachute.png';
+import nhandsalot from './project-images/nhandsalot.png';
+import nkjbk from './project-images/nkjbk.png';
+import reddoor from './project-images/reddoor.jpg';
+import redrunnerrunning from './project-images/redRunnerRunning.png';
+import runthrough from './project-images/runthrough.png';
+import sketchash from './project-images/sketchashCrazinessinsanity.png';
+import tworl from './project-images/tworl.png';
+import zebra from './project-images/zebratest4mask.png';
 
 // thumbas
 import aaThumb from './image-thumbnails/aa.jpg';
-import secondtshirtThumb from './project-images/secondtshirt0053.jpg';
-// import einsteinskeletonThumb from './project-images/einstein-skeleton.jpg';
-// import finalmaskisabigbusinessThumb from './project-images/finalMaskisabigbusiness.jpg';
-// import ghostinthesnowThumb from './project-images/ghost_in_the_snow.jpeg';
-// import landtostandtoheartThumb from './project-images/landtostandtoheart0432.jpg';
-// import movingonlookingaheadparachuteThumb from './project-images/movingonlookingaheadparachute.jpg';
-// import nhandsalotThumb from './project-images/nhandsalot.jpg';
-// import nkjbkThumb from './project-images/nkjbk.jpg';
-// import reddoorThumb from './project-images/reddoor.jpg';
-// import redrunnerrunningThumb from './project-images/redRunnerRunning.jpg';
-// import runthroughThumb from './project-images/runthrough.jpg';
-// import sketchashThumb from './project-images/sketchashCrazinessinsanity.jpg';
-// import tworlThumb from './project-images/tworl.jpg';
-// import zebraThumb from './project-images/zebratest4mask.jpg';
+import secondtshirtThumb from './image-thumbnails/secondtshirt.jpg';
+import einsteinskeletonThumb from './image-thumbnails/einstein-skeleton.jpg';
+import finalmaskisabigbusinessThumb from './image-thumbnails/finalMaskisabigbusiness.jpg';
+import ghostinthesnowThumb from './image-thumbnails/ghost_in_the_snow.jpg';
+import landtostandtoheartThumb from './image-thumbnails/landtostandtoheart0432.jpg';
+import movingonlookingaheadparachuteThumb from './image-thumbnails/movingonlookingaheadparachute.jpg';
+import nhandsalotThumb from './image-thumbnails/nhandsalot.jpg';
+import nkjbkThumb from './image-thumbnails/nkjbk.jpg';
+import reddoorThumb from './image-thumbnails/reddoor.jpg';
+import redrunnerrunningThumb from './image-thumbnails/redRunnerRunning.jpg';
+import runthroughThumb from './image-thumbnails/runthrough.jpg';
+import sketchashThumb from './image-thumbnails/sketchashCrazinessinsanity.jpg';
+import tworlThumb from './image-thumbnails/tworl.jpg';
+import zebraThumb from './image-thumbnails/zebratest4mask.jpg';
 
 export const images = [
   {
@@ -43,69 +43,69 @@ export const images = [
     thumbnail: secondtshirtThumb,
     description: '2ndtshirt'
   },
-  // {
-  //   original: einsteinskeleton,
-  //   thumbnail: einsteinskeletonThumb,
-  //   description: 'einstein skeleton'
-  // },
-  // {
-  //   original: finalmaskisabigbusiness,
-  //   thumbnail: finalmaskisabigbusinessThumb,
-  //   description: 'final mask is a big business'
-  // },
-  // {
-  //   original: ghostinthesnow,
-  //   thumbnail: ghostinthesnowThumb,
-  //   description: 'ghostinthesnow'
-  // },
-  // {
-  //   original: landtostandtoheart,
-  //   thumbnail: landtostandtoheartThumb,
-  //   description: 'land to stand to heart'
-  // },
-  // {
-  //   original: movingonlookingaheadparachute,
-  //   thumbnail: movingonlookingaheadparachuteThumb,
-  //   description: 'moving on looking ahead'
-  // },
-  // {
-  //   original: nhandsalot,
-  //   thumbnail: nhandsalotThumb,
-  //   description: 'a lot of hands'
-  // },
-  // {
-  //   original: nkjbk,
-  //   thumbnail: nkjbkThumb,
-  //   description: 'nkjbk'
-  // },
-  // {
-  //   original: reddoor,
-  //   thumbnail: reddoorThumb,
-  //   description: 'red door'
-  // },
-  // {
-  //   original: redrunnerrunning,
-  //   thumbnail: redrunnerrunningThumb,
-  //   description: 'red runner running'
-  // },
-  // {
-  //   original: runthrough,
-  //   thumbnail: runthroughThumb,
-  //   description: 'run through'
-  // },
-  // {
-  //   original: sketchash,
-  //   thumbnail: sketchashThumb,
-  //   description: 'ash'
-  // },
-  // {
-  //   original: tworl,
-  //   thumbnail: tworlThumb,
-  //   description: 'tworl'
-  // },
-  // {
-  //   original: zebra,
-  //   thumbnail: zebraThumb,
-  //   description: 'zebra'
-  // }
+  {
+    original: einsteinskeleton,
+    thumbnail: einsteinskeletonThumb,
+    description: 'einstein skeleton'
+  },
+  {
+    original: finalmaskisabigbusiness,
+    thumbnail: finalmaskisabigbusinessThumb,
+    description: 'final mask is a big business'
+  },
+  {
+    original: ghostinthesnow,
+    thumbnail: ghostinthesnowThumb,
+    description: 'ghostinthesnow'
+  },
+  {
+    original: landtostandtoheart,
+    thumbnail: landtostandtoheartThumb,
+    description: 'land to stand to heart'
+  },
+  {
+    original: movingonlookingaheadparachute,
+    thumbnail: movingonlookingaheadparachuteThumb,
+    description: 'moving on looking ahead'
+  },
+  {
+    original: nhandsalot,
+    thumbnail: nhandsalotThumb,
+    description: 'a lot of hands'
+  },
+  {
+    original: nkjbk,
+    thumbnail: nkjbkThumb,
+    description: 'nkjbk'
+  },
+  {
+    original: reddoor,
+    thumbnail: reddoorThumb,
+    description: 'red door'
+  },
+  {
+    original: redrunnerrunning,
+    thumbnail: redrunnerrunningThumb,
+    description: 'red runner running'
+  },
+  {
+    original: runthrough,
+    thumbnail: runthroughThumb,
+    description: 'run through'
+  },
+  {
+    original: sketchash,
+    thumbnail: sketchashThumb,
+    description: 'ash'
+  },
+  {
+    original: tworl,
+    thumbnail: tworlThumb,
+    description: 'tworl'
+  },
+  {
+    original: zebra,
+    thumbnail: zebraThumb,
+    description: 'zebra'
+  }
 ]
