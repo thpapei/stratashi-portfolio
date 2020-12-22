281f455ac8601259fa87ba08fb300a3375adcea7
Thomas Pappas <thpapei1@gmail.com>
2020-12-22 15:05:59 +0200

    Created a temporary video gallery component

---
+++ src/images/projects/videos.js
@@ -0,0 +1,29 @@
+import ambiance3d from './video-thumbnails/ambiance3d.jpg';
+import humanConsciousness from './video-thumbnails/HumanConsciousnessProject.jpg';
+import nero from './video-thumbnails/nero.jpg';
+import theWindUpBird from './video-thumbnails/Thewindupbird.jpg';
+
+const items = [
+  {
+    embedUrl: 'r7c_5KNQxok',
+    thumbnail: humanConsciousness,
+    original: humanConsciousness,
+  },
+  {
+    embedUrl: '1uifXl5VfyA',
+    thumbnail: theWindUpBird,
+    original: theWindUpBird,
+  },
+  {
+    embedUrl: '2ILLw56OLIQ',
+    thumbnail: nero,
+    original: nero,
+  },
+  {
+    embedUrl: 'TMXPJklDrhA',
+    thumbnail: ambiance3d,
+    original: ambiance3d,
+  }
+];
+
+export { items };
