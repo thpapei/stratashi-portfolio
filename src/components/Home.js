7ca580b04e8e34dfa3ea19c9e24696bb385ff5d2
Thomas Pappas <thpapei1@gmail.com>
2020-12-23 17:34:01 +0200

    Added social media icons on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,15 @@
+import facebook from '../images/social-media-icons/facebook.png'
+import instagram from '../images/social-media-icons/instagram.png'
+import youtube from '../images/social-media-icons/youtube.png'
+
 const Home = () => {
   return (
     <div className='home' id='home'>
-      <div>
-        <h1 className='name'>Stratashi</h1>
+      <h1 className='name'>Stratashi</h1>
+      <div className='social-media'>
+        <a href='https://www.facebook.com/stratashi' target="_blank" rel='noreferrer'><img src={facebook} alt='facebook' /></a>
+        <a href='' target="_blank" rel='noreferrer'><img src={instagram} alt='instagram' /></a>
+        <a href='https://www.youtube.com/user/Stratashi/featured' target="_blank" rel='noreferrer'><img src={youtube} alt='youtube' /></a>
       </div>
     </div>
   )
