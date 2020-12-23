10c7671d6cadab5c0d3f20cb67f41442da40f840
Thomas Pappas <thpapei1@gmail.com>
2020-12-23 18:26:59 +0200

    Small code clear-ups

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,8 @@
 const About = () => {
   return (
     <div className='about'>
-      <div>
-        <h1 className='title'>About   </h1>
-      </div>
-      <div>
+      <h1 className='title'>About</h1>
+      <div className='info'>
         <h3>My name is Stratashi bla bla bla</h3>
       </div>
     </div>
