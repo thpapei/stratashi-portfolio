46069c7a34a2c1a1981741a377ab586df9119700
Thomas Pappas <thpapei1@gmail.com>
2020-12-26 13:44:16 +0200

    Fixed navbar showing pictures before videos

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,8 +8,8 @@ const Nav = ({ homeRef }) => {
       <ul>
         <li className='active'><Link to='home' spy={true} smooth={true} offset={-100} duration={500}>Home</Link></li>
         <li><Link to='about' spy={true} smooth={true} offset={0} duration={500}>About</Link></li>
-        <li><Link to='pictures' spy={true} smooth={true} offset={0} duration={500}>Pictures</Link></li>
         <li><Link to='videos' spy={true} smooth={true} offset={0} duration={500}>Videos</Link></li>
+        <li><Link to='pictures' spy={true} smooth={true} offset={0} duration={500}>Pictures</Link></li>
         <li><Link to='contact' spy={true} smooth={true} offset={0} duration={500}>Contact</Link></li>
       </ul>
     </div >
