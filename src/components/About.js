ec28381ba2e9f526e50b386f83dd669690364be6
Thomas Pappas <thpapei1@gmail.com>
2020-12-26 13:43:08 +0200

    Removed blue background in About section and changed font-family

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ const About = () => {
     <div className='about'>
       <h1 className='title'>About</h1>
       <div className='info'>
-        <h3>My name is Stratashi bla bla bla</h3>
+        <p>My name is stratashi</p>
       </div>
     </div>
   )
